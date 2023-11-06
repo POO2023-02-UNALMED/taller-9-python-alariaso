cafba4ff7e912721ae9fd6529d3dc65085220d8c
alariaso <141962262+alariaso@users.noreply.github.com>
2023-11-06 16:15:03 -0500

    operaciones solo entre 2 numeros

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from tkinter import Tk, Button, Entry, END
 
-numeros = []
-operadores = []
-ultimo_fue_operador = False
+numero1 = ""
+numero2 = ""
+operador = ""
 
 # Configuración ventana principal
 root = Tk()
@@ -16,74 +16,56 @@ pantalla.grid(row=0, column=0, columnspan=4, padx=1, pady=1)
 
 
 def calcular_resultado():
-    global ultimo_fue_operador
-    global numeros
-    global operadores
+    global numero1
+    global numero2
+    global operador
 
-    if len(operadores) + 1 != len(numeros):
+    if not all([numero1, numero2, operador]):
         return
 
     resultado = 0.0
 
-    ops = ["*", "/"]
+    n1 = float(numero1)
+    n2 = float(numero2)
 
-    while len(operadores) > 0:
-        idx = 0
-        while idx < len(operadores):
-            op = operadores[idx]
-            if op in ops:
-                n1 = float(numeros[idx])
-                n2 = float(numeros[idx+1])
+    if operador == "*":
+        resultado = n1*n2
+    elif operador == "/":
+        resultado = n1/n2
+    elif operador == "+":
+        resultado = n1+n2
+    elif operador == "-":
+        resultado = n1-n2
 
-                operadores.pop(idx)
-                numeros.pop(idx)
-
-                if op == "*":
-                    resultado = n1*n2
-                elif op == "/":
-                    resultado = n1/n2
-                elif op == "+":
-                    resultado = n1+n2
-                elif op == "-":
-                    resultado = n1-n2
-
-                numeros[idx] = str(resultado)
-                idx = 0
-            else:
-                idx += 1
-        if ops == ["*", "/"] and len(list(filter(lambda op: op in ops, operadores))) == 0:
-            ops = ["+", "-"]
-
-    numeros = [str(resultado)]
-    operadores = []
-    pantalla.delete(0, END)
-    pantalla.insert("end", str(resultado))
+    numero1 = str(resultado)
+    numero2 = ""
+    operador = ""
+    pintar_pantalla()
 
 
 def manejar_click(texto):
-    global ultimo_fue_operador
-    global numeros
-    global operadores
+    global numero1
+    global numero2
+    global operador
 
     if texto == "=":
         calcular_resultado()
         return
 
-    pantalla.insert("end", texto)
     if texto in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "."]:
-        if ultimo_fue_operador or len(numeros) == 0:
-            numeros.append(texto)
+        if operador:
+            numero2 += texto
         else:
-            numeros[-1] += texto
-        ultimo_fue_operador = False
+            numero1 += texto
     else:
-        if ultimo_fue_operador:
-            operadores[-1] = texto
-            l = len(pantalla.get())
-            pantalla.delete(l-2, l-1)
-        else:
-            operadores.append(texto)
-        ultimo_fue_operador = True
+        operador = texto
+    
+    pintar_pantalla()
+
+
+def pintar_pantalla():
+    pantalla.delete(0, END)
+    pantalla.insert(END, numero1+operador+numero2)
 
 # Configuración botones
 boton_1 = Button(root, text="1", width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2", command=lambda: manejar_click("1")).grid(row=1, column=0, padx=1, pady=1)
